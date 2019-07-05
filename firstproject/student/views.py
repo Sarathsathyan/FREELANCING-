d81c7059b4cac46b75a71f0a710b265011065ebd
Sarathsathyan <sarathsathyan98@gmail.com>
2019-07-06 00:15:00 +0530

    after client

--- firstproject/student/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def dashboard(request):
-    return render(request,'student/dashboard.html')
-
-# Create your views here.
